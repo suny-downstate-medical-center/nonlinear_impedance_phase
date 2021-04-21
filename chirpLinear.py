fixed impedance calculation, updated code for comparison figure

chirpLinear.py
@@ -1,7 +1,7 @@
 from getCells import M1Cell   
 s = M1Cell()  
 seg = s.net.cells[0].secs['soma']['hObj'](0.5)  
-from chirpUtils import getChirpLog
+from chirpUtils import getChirp
 from neuron import h 
 stim = h.IClamp(seg)
 from pylab import fft, convolve
@@ -18,7 +18,7 @@ f0 = 0.5
 f1 = 500 #20
 soma_v = h.Vector().record(seg._ref_v) 
 time = h.Vector().record(h._ref_t)
-I, t = getChirpLog(f0, f1, t0, amp, Fs, delay)
+I, t = getChirp(f0, f1, t0, amp, Fs, delay)
 i = h.Vector().record(h.IClamp[0]._ref_i)
 stim.amp = 0
 stim.dur = (t0+delay*2) * Fs + 1
@@ -37,7 +37,7 @@ current = np.hstack((np.repeat(current[0],int(delay*sampr)),current, np.repeat(c
 current = current - np.mean(current) 
 #v = v[int(delay*sampr - 0.5*sampr)+1:-int(delay*sampr - 0.5*sampr)] 
 v = v - np.mean(v) 
-v = np.hstack((np.repeat(v[0],int(delay*sampr)), v, np.repeat(0, int(delay*sampr)))) 
+v = np.hstack((np.repeat(0,int(delay*sampr)), v, np.repeat(0, int(delay*sampr)))) 
 f_current = (fft(current)/len(current))[0:int(len(current)/2)] 
 f_cis = (fft(v)/len(v))[0:int(len(v)/2)] 
 z = f_cis / f_current 

compareChirpNoise.py
@@ -7,16 +7,17 @@ from scipy.io import loadmat
 
 # loading and preprocessing 
 ## chirp and noise data
-noise = loadmat('data/noise_20s_25ptSmth.mat')
-chirp = loadmat('data/chirp_amp025_T20.mat')
-chirpLong = loadmat('data/chirp_05-500Hz_60s.mat')
-chirpLog20 = loadmat('data/log_chirp_20s_05-20Hz.mat')
-chirpLog60 = loadmat('data/log_chirp_60s_05-500Hz.mat')
-
-## trim noise 
-noise_freqs_cut = [f for f in noise['Freq'][0] if 0.5 <= f <= 20.0]
-noise_zAmp_cut = [z for f, z in zip(noise['Freq'][0], noise['ZinAmp'][0]) if 0.5 <= f <= 20.0]
-noise_zPhase_cut = [z for f, z in zip(noise['Freq'][0], noise['ZinPhase'][0]) if 0.5 <= f <= 20.0]
+# noise = loadmat('data/noise_20s_25ptSmth.mat')
+# chirp = loadmat('data/chirp_amp025_T20.mat')
+# chirpLong = loadmat('data/chirp_05-500Hz_60s.mat')
+# chirpLog20 = loadmat('data/log_chirp_20s_05-20Hz.mat')
+# chirpLog60 = loadmat('data/log_chirp_60s_05-500Hz.mat')
+chirp = loadmat('data/chirp_05-20_20s_noFliter.mat')
+noise = loadmat('data/noise_05-500_20s_5std_noFilter.mat')
+chirpLog20 = loadmat('data/logChirp_05-20_20s_noFilter.mat')
+chirpLog60 = loadmat('data/logChirp_05-500_60s_noFilter.mat')
+chirpLong = loadmat('data/chirp_05-500_60s_noFliter.mat')
+noise20 = loadmat('data/noise_05-20_20s_5std_noFilter.mat')
 
 ## noise validation data 
 with open('data/noise_freq_validation_amp01.json', 'rb') as fileObj:
@@ -29,37 +30,58 @@ for f in data.keys():
     freqs.append(float(f)) 
     zAmp.append(data[f]['zAmp']) 
     zPhase.append(data[f]['zPhase']) 
-validNoise_freqs = [f for f in sorted(freqs)] 
-validNoise_zAmp = [z for f, z in sorted(zip(freqs, zAmp))] 
-validNoise_zPhase = [z for f, z in sorted(zip(freqs, zPhase))] 
-validNoise_freqs_cut = [f for f in sorted(freqs) if 0.5 <= f <= 20.0] 
-validNoise_zAmp_cut = [z for f, z in sorted(zip(freqs, zAmp)) if 0.5 <= f <= 20.0] 
-validNoise_zPhase_cut = [z for f, z in sorted(zip(freqs, zPhase)) if 0.5 <= f <= 20.0] 
+noise_freqs = [f for f in sorted(freqs)] 
+noise_zAmp = [z for f, z in sorted(zip(freqs, zAmp))] 
+noise_zPhase = [z for f, z in sorted(zip(freqs, zPhase))] 
+
+validNoise_freqs = []
+validNoise_zAmp = []
+validNoise_zPhase = []
+for f in noise['Freq'][0]:
+    ind = np.argmin(np.square(np.subtract(noise_freqs,f)))
+    validNoise_freqs.append(noise_freqs[ind])
+    validNoise_zAmp.append(noise_zAmp[ind])
+    validNoise_zPhase.append(noise_zPhase[ind])
+
+validNoise_freqs_cut = []
+validNoise_zAmp_cut = []
+validNoise_zPhase_cut = []
+for f in noise20['Freq'][0]:
+    ind = np.argmin(np.square(np.subtract(noise_freqs,f)))
+    validNoise_freqs_cut.append(noise_freqs[ind])
+    validNoise_zAmp_cut.append(noise_zAmp[ind])
+    validNoise_zPhase_cut.append(noise_zPhase[ind])
+
 
 ## chirp validation data 
-with open('data/chirp_freq_validation_amp01.json', 'rb') as fileObj:
-    data = json.load(fileObj)
+# with open('data/chirp_freq_validation_amp01.json', 'rb') as fileObj:
+#     data = json.load(fileObj)
 
-freqs = [] 
-zAmp = [] 
-zPhase = [] 
-for f in data.keys(): 
-    freqs.append(float(f)) 
-    zAmp.append(data[f]['zAmp']) 
-    zPhase.append(data[f]['zPhase']) 
-validChirp_freqs = [f for f in sorted(freqs)] 
-validChirp_zAmp = [z for f, z in sorted(zip(freqs, zAmp))] 
-validChirp_zPhase = [z for f, z in sorted(zip(freqs, zPhase))] 
+# freqs = [] 
+# zAmp = [] 
+# zPhase = [] 
+# for f in data.keys(): 
+#     freqs.append(float(f)) 
+#     zAmp.append(data[f]['zAmp']) 
+#     zPhase.append(data[f]['zPhase']) 
+validChirp_freqs = [] # [f for f in sorted(freqs)] 
+validChirp_zAmp = [] # [z for f, z in sorted(zip(freqs, zAmp))] 
+validChirp_zPhase = [] # [z for f, z in sorted(zip(freqs, zPhase))] 
+for f in chirp['Freq'][0]:
+    ind = np.argmin(np.square(np.subtract(noise_freqs,f)))
+    validChirp_freqs.append(noise_freqs[ind])
+    validChirp_zAmp.append(noise_zAmp[ind])
+    validChirp_zPhase.append(noise_zPhase[ind])
 
 ## create valid set for long chirp 
 validChirpLong_zAmp = []
 validChirpLong_zPhase = []
 validChirpLong_freq = []
 for f in chirpLong['Freq'][0]:
-    ind = np.argmin(np.square(np.subtract(validNoise_freqs,f)))
-    validChirpLong_freq.append(validNoise_freqs[ind])
-    validChirpLong_zAmp.append(validNoise_zAmp[ind])
-    validChirpLong_zPhase.append(validNoise_zPhase[ind])
+    ind = np.argmin(np.square(np.subtract(noise_freqs,f)))
+    validChirpLong_freq.append(noise_freqs[ind])
+    validChirpLong_zAmp.append(noise_zAmp[ind])
+    validChirpLong_zPhase.append(noise_zPhase[ind])
 
 ## valid set for log chirp 
 validChirpLog20_zAmp = []
@@ -69,21 +91,21 @@ validChirpLog60_zAmp = []
 validChirpLog60_zPhase = []
 validChirpLog60_freq = []
 for f in chirpLog20['Freq'][0]:
-    ind = np.argmin(np.square(np.subtract(validNoise_freqs, f)))
-    validChirpLog20_zAmp.append(validNoise_zAmp[ind])
-    validChirpLog20_zPhase.append(validNoise_zPhase[ind])
-    validChirpLog20_freq.append(validNoise_freqs[ind])
+    ind = np.argmin(np.square(np.subtract(noise_freqs, f)))
+    validChirpLog20_zAmp.append(noise_zAmp[ind])
+    validChirpLog20_zPhase.append(noise_zPhase[ind])
+    validChirpLog20_freq.append(noise_freqs[ind])
 for f in chirpLog60['Freq'][0]:
-    ind = np.argmin(np.square(np.subtract(validNoise_freqs, f)))
-    validChirpLog60_zAmp.append(validNoise_zAmp[ind])
-    validChirpLog60_zPhase.append(validNoise_zPhase[ind])
-    validChirpLog60_freq.append(validNoise_freqs[ind])
+    ind = np.argmin(np.square(np.subtract(noise_freqs, f)))
+    validChirpLog60_zAmp.append(noise_zAmp[ind])
+    validChirpLog60_zPhase.append(noise_zPhase[ind])
+    validChirpLog60_freq.append(noise_freqs[ind])
 
 # compute errors 
-noise_zAmpErr = mean_squared_error(validNoise_zAmp, noise['ZinAmp'][0])
-noise_zPhaseErr = mean_squared_error(validNoise_zPhase, noise['ZinPhase'][0])
-noise_cut_zAmpErr = mean_squared_error(noise_zAmp_cut, validNoise_zAmp_cut)
-noise_cut_zPhaseErr = mean_squared_error(noise_zPhase_cut, validNoise_zPhase_cut)
+noise_zAmpErr = mean_squared_error(noise['ZinAmp'][0], validNoise_zAmp)
+noise_zPhaseErr = mean_squared_error(noise['ZinPhase'][0], validNoise_zPhase)
+noise_cut_zAmpErr = mean_squared_error(noise20['ZinAmp'][0], validNoise_zAmp_cut)
+noise_cut_zPhaseErr = mean_squared_error(noise20['ZinPhase'][0], validNoise_zPhase_cut)
 chirp_zAmpErr = mean_squared_error(chirp['ZinAmp'][0], validChirp_zAmp)
 chirp_zPhaseErr = mean_squared_error(chirp['ZinPhase'][0], validChirp_zPhase)
 longChirp_zAmpErr = mean_squared_error(chirpLong['ZinAmp'][0], validChirpLong_zAmp)
@@ -99,7 +121,7 @@ plt.subplot(2,2,1)
 l = r'Log Chirp: %.2f M$\Omega^2$' % (logChirp20_zAmpErr)
 plt.plot(chirpLog20['Freq'][0], chirpLog20['ZinAmp'][0], label=l, color='g')
 l = r'Noise: %.2f M$\Omega^2$' % (noise_cut_zAmpErr)
-plt.plot(noise_freqs_cut, noise_zAmp_cut, label=l, color='r')
+plt.plot(noise20['Freq'][0], noise20['ZinAmp'][0], label=l, color='r')
 l = r'Linear Chirp: %.2f M$\Omega^2$' % (chirp_zAmpErr)
 plt.plot(chirp['Freq'][0], chirp['ZinAmp'][0], label=l, color='b')
 plt.plot(validChirp_freqs, validChirp_zAmp, label='Validation', color='k')
@@ -114,7 +136,7 @@ plt.subplot(2,2,2)
 l = r'Log Chirp: %.5f radians$^2$' % (logChirp20_zPhaseErr)
 plt.plot(chirpLog20['Freq'][0], chirpLog20['ZinPhase'][0], label=l, color='g')
 l = r'Noise: %.5f radians$^2$' % (noise_cut_zPhaseErr)
-plt.plot(noise_freqs_cut, noise_zPhase_cut, label=l, color='r')
+plt.plot(noise20['Freq'][0], noise20['ZinPhase'][0], label=l, color='r')
 l = r'Linear Chirp: %.5f radians$^2$' % (chirp_zPhaseErr)
 plt.plot(chirp['Freq'][0], chirp['ZinPhase'][0], label=l, color='b')
 plt.plot(validChirp_freqs, validChirp_zPhase, label='Validation', color='k')
@@ -145,11 +167,11 @@ plt.xticks(fontsize=12)
 plt.yticks(fontsize=12)
 
 plt.subplot(2,2,4)
-l = r'Noise: %.2f radians$^2$' % (noise_zPhaseErr)
+l = r'Noise: %.5f radians$^2$' % (noise_zPhaseErr)
 plt.semilogx(noise['Freq'][0], noise['ZinPhase'][0], label=l, color='r')
-l = r'Log Chirp: %.3f radians$^2$' % (logChirp60_zPhaseErr)
+l = r'Log Chirp: %.5f radians$^2$' % (logChirp60_zPhaseErr)
 plt.semilogx(chirpLog60['Freq'][0], chirpLog60['ZinPhase'][0], label=l, color='g')
-l = r'Linear Chirp: %.2f radians$^2$' % (longChirp_zPhaseErr)
+l = r'Linear Chirp: %.5f radians$^2$' % (longChirp_zPhaseErr)
 # plt.plot(chirpLong['Freq'][0], chirpLong['ZinPhase'][0], label=l, color='b')
 # l = r'Noise: %.2f radians$^2$' % (noise_zPhaseErr)
 # plt.plot(noise['Freq'][0], noise['ZinPhase'][0], label=l, color='r')

gaussNoise.py
@@ -1,7 +1,7 @@
 from getCells import M1Cell   
 s = M1Cell()  
 seg = s.net.cells[0].secs['soma']['hObj'](0.5)  
-from chirpUtils import getChirpLog
+from chirpUtils import getChirpLog, getNoise
 from neuron import h 
 stim = h.IClamp(seg)
 from pylab import fft, convolve
@@ -10,15 +10,16 @@ import numpy as np
 import os
 
 amp = 0.02 
-t0 = 60 #20
+t0 = 2 #20
 delay = 5
 Fs = 1000
 sampr = 40e3 
 f0 = 0.5
-f1 = 500 #20
+f1 = 20
 soma_v = h.Vector().record(seg._ref_v) 
 time = h.Vector().record(h._ref_t)
-I, t = getChirpLog(f0, f1, t0, amp, Fs, delay)
+# I, t = getChirpLog(f0, f1, t0, amp, Fs, delay)
+I, t = getNoise(0.0, 0.5, t0, 0.01, Fs, delay)
 i = h.Vector().record(h.IClamp[0]._ref_i)
 stim.amp = 0
 stim.dur = (t0+delay*2) * Fs + 1
@@ -31,13 +32,13 @@ v_trim = [v for v, T in zip(soma_v, time) if int((delay)*1000) < T < int((delay+
 i_trim = [x for x, T in zip(i,time) if int((delay)*1000) < T < int((delay+t0)*1000)] 
 time_trim = [T for v, T in zip(soma_v, time) if int((delay)*1000) < T < int((delay+t0)*1000)] 
 current = i_trim
-v = v_trim 
+v = v_trim
 #current = current[int(delay*sampr - 0.5*sampr+1):-int(delay*sampr- 0.5*sampr)] 
-current = np.hstack((np.repeat(current[0],int(delay*sampr)),current, np.repeat(current[-1], int(delay*sampr)))) 
 current = current - np.mean(current) 
+current = np.hstack((np.repeat(0,int(delay*sampr)),current, np.repeat(0, int(delay*sampr)))) 
 #v = v[int(delay*sampr - 0.5*sampr)+1:-int(delay*sampr - 0.5*sampr)] 
-v = v - np.mean(v) 
-v = np.hstack((np.repeat(v[0],int(delay*sampr)), v, np.repeat(0, int(delay*sampr)))) 
+v = v - np.mean(v)
+v = np.hstack((np.repeat(0,int(delay*sampr)), v, np.repeat(0, int(delay*sampr))))  
 f_current = (fft(current)/len(current))[0:int(len(current)/2)] 
 f_cis = (fft(v)/len(v))[0:int(len(v)/2)] 
 z = f_cis / f_current 
@@ -53,7 +54,7 @@ Qfactor    = zResAmp / zamp[0]
 fVar       = np.std(zamp) / np.mean(zamp)
 peak_to_peak = np.max(v) - np.min(v)
 ## smoothing
-bwinsz = 1
+bwinsz = 1 #5
 fblur = np.array([1.0/bwinsz for i in range(bwinsz)])
 zamp = convolve(zamp,fblur,'same')
 phase = convolve(phase, fblur, 'same')