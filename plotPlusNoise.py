2fd20f6ed974bf4595c1a0b3d4baea64dd092dee
craig <kelley92@bu.edu>
2021-04-26 10:24:42 -0400

    plotPlusNoise.py: v0.01 - adding optimal filter window size

--- plotPlusNoise.py
+++ plotPlusNoise.py
@@ -16,19 +16,34 @@ noise_v_std = np.zeros((len(noise_amps), len(noise_stds)))
 noise_v_amp = np.zeros((len(noise_amps), len(noise_stds)))
 zPhaseErrs = np.zeros((len(noise_amps), len(noise_stds)))
 zAmpErrs = np.zeros((len(noise_amps), len(noise_stds)))
+optFiltAmp = np.zeros((len(noise_amps), len(noise_stds)))
+optFiltPhs = np.zeros((len(noise_amps), len(noise_stds)))
 
 # plot error for each std - amp pair
 for filename in filenames:
     noise_amp = float(filename.split('-')[1])
     noise_std = float(filename.split('-')[3])
     bnsz = int(filename.split('-')[-1].split('.')[0])
+    amperrs = []
+    phserrs = []
     if bnsz == 1:
         data = loadmat('data/chirpNoiseData/' + filename)
         xind = np.argmin(np.square(np.subtract(noise_amps,noise_amp)))
         yind = np.argmin(np.square(np.subtract(noise_stds, noise_std)))
-        zPhaseErrs[xind][yind] = data['zPhaseErr'][0][0]
-        zAmpErrs[xind][yind] = data['zAmpErr'][0][0]
-        noise_v_std[xind][yind] = data['noise_v_std'][0]
+        zPhaseErrs[xind][yind] = data['zPhaseErr']
+        zAmpErrs[xind][yind] = data['zAmpErr']
+        noise_v_std[xind][yind] = data['noise_v_std']
         noise_v_amp[xind][yind] = data['noise_peak_to_peak']
+        amperrs.append(data['zAmpErr'])
+        phserrs.append(data['zPhaseErr'])
+        for bnsz in binsizes[1:]:
+            data = loadmat('data/chirpNoiseData/' + filename.split('bnsz')[0] 
+                + 'bnsz-' + str(bnsz) + '.mat')
+            amperrs.append(data['zAmpErr'])
+            phserrs.append(data['zPhaseErr'])
+        optFiltAmp[xind][yind] = binsizes[np.argmin(amperrs)]
+        optFiltPhs[xind][yind] = binsizes[np.argmin(phserrs)]
+
+
 
 # v0.00 - plot amplitude and phase errs by amp and std of noise 
